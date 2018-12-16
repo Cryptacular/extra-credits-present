72e37e3d62ad0218406783cf5af676c5dc869eda
Nick Mertens <nick@thecrypt.co.nz>
2018-12-16 19:40:34 +1300

    Fix character animation. Add jump ability.

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -1,17 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CharacterController : MonoBehaviour {
-    public float Speed = 10.0f;
-    public bool IsWalking = false;
+    public float Speed = 10f;
+    public float JumpForce = 10f;
+    private Animator animator;
 
-	void Update () {
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update ()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
         var x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
         transform.Translate(x, 0, 0);
-        IsWalking = System.Math.Abs(x) > 0.1;
 
-        var IsFacingLeft = x < 0;
-        this.GetComponent<SpriteRenderer>().flipX = IsFacingLeft;
+        var isWalking = System.Math.Abs(x) > 0.1;
+        animator.SetBool("IsWalking", isWalking);
+
+        var isFacingLeft = x < 0;
+        this.GetComponent<SpriteRenderer>().flipX = isFacingLeft;
+    }
+
+    private void Jump()
+    {
+        var isJumping = Input.GetKeyDown(KeyCode.Space);
+        var rb = GetComponent<Rigidbody2D>();
+
+        var isTouchingFloor = System.Math.Abs(rb.velocity.y) < 0.01;
+
+        if (isJumping && isTouchingFloor)
+        {
+            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
     }
 }
