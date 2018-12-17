b4e9223f89c67b5bce9e1a64220172decebc9488
Nick Mertens <nickemans@gmail.com>
2018-12-17 20:38:25 +1300

    Reset character when touching death collider

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -1,16 +1,18 @@
 ﻿using UnityEngine;
 
-public class CharacterController : MonoBehaviour {
+public class CharacterController : MonoBehaviour
+{
     public float Speed = 10f;
     public float JumpForce = 10f;
     private Animator animator;
+    private Vector2 positionBeforeJump;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    void Update ()
+    void Update()
     {
         Move();
         Jump();
@@ -35,9 +37,32 @@ public class CharacterController : MonoBehaviour {
 
         var isTouchingFloor = System.Math.Abs(rb.velocity.y) < 0.01;
 
+        if (isTouchingFloor)
+        {
+            positionBeforeJump = gameObject.transform.position;
+        }
+
         if (isJumping && isTouchingFloor)
         {
             rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void Die()
+    {
+        gameObject.transform.position = positionBeforeJump;
+        var rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var name = collision.collider.name;
+        switch(name)
+        {
+            case "Death":
+                Die();
+                break;
         }
     }
 }
