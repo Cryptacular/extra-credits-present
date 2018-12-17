403d44ddbe2498a5f7340388710186f1c652236d
Lorrigan <lorrigan269@gmail.com>
2018-12-17 20:12:06 +1300

    added a camera to the player level. Will need to adjust the fall

---
+++ Assets/PlayerFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+    private Vector2 velocity;
+
+    public float smoothTimeX, smoothTimeY;
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
