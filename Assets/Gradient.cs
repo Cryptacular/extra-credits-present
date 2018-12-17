e11c44b085dc405598389e4ac7488d0fd4ed5fce
Sheldon Nunes <sheldon.nunes02@gmail.com>
2018-12-17 22:37:52 +1300

    Change adds gradient for rain

---
+++ Assets/Gradient.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gradient : MonoBehaviour
+{
+    public static Color blue = new Color(197, 239, 242);
+
+    public UnityEngine.UI.RawImage img;
+
+    private Texture2D backgroundTexture;
+
+    void Awake()
+    {
+        backgroundTexture = new Texture2D(1, 2);
+        backgroundTexture.wrapMode = TextureWrapMode.Clamp;
+        backgroundTexture.filterMode = FilterMode.Bilinear;
+        SetColor(blue, Color.gray);
+    }
+
+    public void SetColor(Color color1, Color color2)
+    {
+        backgroundTexture.SetPixels(new Color[] { color1, color2 });
+        backgroundTexture.Apply();
+        img.texture = backgroundTexture;
+    }
+}
