20cf6bede262fb00fae7ede95641a8ceb4961f8e
Nick Mertens <nick@thecrypt.co.nz>
2018-12-18 08:37:59 +1300

    Add rising water

---
+++ Assets/WaterTriggerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class WaterTriggerController : MonoBehaviour
+{
+    public GameObject initiator;
+    public GameObject water;
+    private bool activated = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == initiator && !activated)
+        {
+            activated = true;
+            var rb = water.GetComponent<Rigidbody2D>();
+
+            rb.velocity = new Vector2(0, 1);
+        }
+    }
+
+}
